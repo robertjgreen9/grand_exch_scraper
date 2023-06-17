a19f3f6ac53cd95bbcc8344586227bc4766572ab
robertjgreen9 <92520225+robertjgreen9@users.noreply.github.com>
2023-06-17 23:24:18 +0100

    Updated method of accessing database and logic for writing responses.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
-from dotenv import dotenv_values
+from dotenv import dotenv_values, load_dotenv
+load_dotenv()
 from pathlib import Path
-from mysql.connector import Error
-import mysql.connector
 import requests
-from ids import ids 
+from ids import ids
+import ssl
+import pymysql.cursors
+import time
+import json
+
 
 url = "https://api.weirdgloop.org/exchange/history/rs/latest?id="
 
@@ -13,23 +17,17 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 dotenv_path = f"{current_directory}/.env"
 config = dotenv_values(dotenv_path)
 
-# Opens the connection to the server in the config .env
-connection = mysql.connector.connect(
-    host = config["HOST"],
-    database = config["DATABASE"],
-    user = config["USERNAME"],
-    password = config["PASSWORD"],
-    ssl_ca = os.getenv("SSL_CERT")
+connection = pymysql.connect(
+  host= os.getenv("HOST"),
+  user=os.getenv("USERNAME"),
+  passwd= os.getenv("PASSWORD"),
+  db= os.getenv("DATABASE"),
+  ssl_ca = "/etc/ssl/certs/ca-certificates.crt",
+  cursorclass = pymysql.cursors.DictCursor
 )
 
 # Creates the cursor object, which is the method for interacting with the database
-try:
-    if connection.is_connected():
-        cursor = connection.cursor()
-    else:
-        print('Not connected.')
-except Error as e:
-    print("Error while connecting to MySQL", e)
+cursor = connection.cursor()
 
 
 # Load the GE API's JSON for each ID
@@ -43,6 +41,16 @@ def parse_json(json, id):
     id, price, timestamp = keys['id'], keys['price'], keys['timestamp']
     return id, price, timestamp
 
+def load_timestamp_json():
+    with open("/home/pi/grand_exch_scraper/last_time_stamp.json", "r") as jsonFile:
+        timestamp_json = json.load(jsonFile)
+    timestamp_record = timestamp_json
+    return timestamp_record
+
+def update_timestamp_json(new_timestamp):
+    with open("/home/pi/grand_exch_scraper/last_time_stamp.json", "w") as jsonFile:
+        json.dump(new_timestamp, jsonFile)
+
 # Testing the write and commit
 def write_to_table(cursor, id, price, timestamp):
     sql = "INSERT INTO grand_exchange_data (id, price, timestamp) VALUES (%s, %s, %s)"
@@ -50,9 +58,17 @@ def write_to_table(cursor, id, price, timestamp):
     cursor.execute(sql, val)
     connection.commit()
 
-for i in range(0, len(ids)):
-    json = load_json(url, ids[i])
-    id, price, timestamp = parse_json(json, ids[i])
-    write_to_table(cursor, id, price, timestamp)
+def run_script():
+    old_timestamp = str(load_timestamp_json())
+    for i in range(0, len(ids)):
+        json = load_json(url, ids[i])
+        id, price, timestamp = parse_json(json, ids[i])
+        if timestamp == old_timestamp:
+            pass
+        else:
+            write_to_table(cursor, id, price, timestamp)
+    update_timestamp_json(timestamp)
+    
+run_script()
 
 connection.close()
